daecb2ab81451a2ce7c46a0371e820942e470605
samiyagmur <77567437+samiyagmur@users.noreply.github.com>
2022-03-13 14:30:33 +0300

    master

---
+++ Dat.7.Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAt._7.Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
