08884178022652657e43be5dd37b73c525ccd14e
samiyagmur <77567437+samiyagmur@users.noreply.github.com>
2022-03-13 14:30:19 +0300

    master

--- Sum.Array.Sum/Program.cs
+++ Sum.Array.Sum/Program.cs
@@ -12,44 +12,21 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Result
-{
 
-    /*
-     * Complete the 'simpleArraySum' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY ar as parameter.
-     */
-
-    public static int simpleArraySum(List<int> ar)
-    {
-        int count = ar.Count;
-        int sum = 0;
-        for(int i=0 ; i<count ; i++)
-        {
-            sum+=ar[i];
-        }
-        return sum;
-    }
-
-}
 
 class Solution
 {
     public static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
-
-        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
-
-        int result = Result.simpleArraySum(ar);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        
+        arr.Reverse();
+        
+        foreach(var say in arr)
+            Console.Write(say+" ");
+        
+        
     }
 }
