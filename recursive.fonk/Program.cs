a315f9437b8730e18652ae1dd40700f62c8ee2e9
samiyagmur <samiyagmur92@gmail.com>
2022-03-15 01:26:44 +0300

    commit

---
+++ recursive.fonk/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n)
+    {   //Base case
+        if(n<=1)
+        {
+            return 0;
+        }
+        //recursive case:
+        //for example: First loop n=5
+        //5*factorial(4)
+        //4*factorial(3)
+        //3*factorial(2)
+        //2*factorial(1)
+        //At last We can fix to 5*4*3*2*1=120 That's Faktoriel bayb
+        
+        
+        else
+        {
+            return n*factorial(n-1);
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.factorial(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
