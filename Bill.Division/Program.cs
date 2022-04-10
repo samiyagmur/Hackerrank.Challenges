4fa6ca60c75f11ef3e1c61eb05c4485b70876518
samiyagmur <samiyagmur92@gmail.com>
2022-04-10 15:03:23 +0300

    Prepare for Patika.Dev Bootcamp

---
+++ Bill.Division/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace Bill.Division
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'bonAppetit' function below.
+         *
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY bill
+         *  2. INTEGER k
+         *  3. INTEGER b
+         */
+
+        public static void bonAppetit(List<int> bill, int k, int b)
+        {
+            bill.RemoveAt(k);
+            int cost = 0;
+            int annaPayment;
+            int brainPayment;
+            for (int i = 0; i < bill.Count; i++)
+            {
+                cost += bill[i];
+
+            }
+            brainPayment = (cost / 2);
+
+            if (brainPayment == b)
+            {
+                Console.WriteLine("Bon Appetit");
+            }
+            else
+            {
+                brainPayment = b - brainPayment;
+                Console.WriteLine(brainPayment);
+            }
+
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+            int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.bonAppetit(bill, k, b);
+        }
+    }
+
+}
---
+++ Breaking.the.Records/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+
+namespace Breaking.the.Records
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'breakingRecords' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY scores as parameter.
+         */
+
+        public static List<int> breakingRecords(List<int> scores)
+        {
+            int countMax = 0;
+            int countMin = 0;
+            int min = 0;
+            int max = 0;
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (i == 0)
+                {
+                    min = scores[i];
+                    max = scores[i];
+                }
+                if (scores[i] < min)
+                {
+                    min = scores[i];
+                    countMin++;
+
+                }
+                if (scores[i] > max)
+                {
+                    max = scores[i];
+                    countMax++;
+                }
+            }
+            scores.Clear();
+            scores.Add(countMin);
+            scores.Add(countMax);
+
+            return scores;
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+            List<int> result = Result.breakingRecords(scores);
+
+            Console.WriteLine(String.Join(" ", result));
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
